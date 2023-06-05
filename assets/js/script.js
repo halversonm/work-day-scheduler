6c735b079fa44f5767dec3259adcd4a1e00a8237
mh <maxhalverson@live.com>
2023-06-05 09:54:10 -0500

    set up save button and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,31 @@
 var saveBtnEl = $('.saveBtn');
 var currentTime = $('#currentDay');
-var timeBlock = $('.time-block')
+var timeBlock = $('.time-block');
 
 $(function () {
     var currentHour = dayjs().hour();
-    var date = dayjs().format('dddd, MMMM DD YYYY')
+    var date = dayjs().format('dddd, MMMM DD YYYY');
     currentTime.text(date);
     
     timeBlock.each(function() {
-        var id = $(this).attr('id')
+        var id = $(this).attr('id');
         if(id < currentHour) {
-            $(this).children(".description").attr("class", "col-8 col-md-10 description past")
+            $(this).children(".description").attr("class", "col-8 col-md-10 description past");
         } else if(id > currentHour) {
-            $(this).children(".description").attr("class", "col-8 col-md-10 description future")
+            $(this).children(".description").attr("class", "col-8 col-md-10 description future");
         } else {
-            $(this).children(".description").attr("class", "col-8 col-md-10 description present")
-        }
-    })
+            $(this).children(".description").attr("class", "col-8 col-md-10 description present");
+        };
+    });
 
+    saveBtnEl.on("click", function() {
+        var key = $(this).parent().attr('id');
+        var value = $(this).siblings('.description').val();
+        localStorage.setItem(key, JSON.stringify(value));
+    });
+
+    for(var i = 9; i <= 17; i++) {
+        $(`#${i} textarea`).val(JSON.parse(localStorage.getItem(`${i}`)));
+    };
 });
   
